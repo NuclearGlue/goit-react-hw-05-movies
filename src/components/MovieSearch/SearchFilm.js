33b646202be370f39a9aa252fa323de556973a6d
Alexandr Sorokin <sanek1678@gmail.com>
2022-11-01 13:21:23 +0200

    Update SearchFilm.js

--- src/components/MovieSearch/SearchFilm.js
+++ src/components/MovieSearch/SearchFilm.js
@@ -15,7 +15,7 @@ export default function SearchFilm() {
     if (queueName !== '') {
       getFilms();
     }
-  }, []);
+  });
 
   const getFilms = async () => {
     const result = await fetchFilms(queueName);
